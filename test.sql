4ddf830e4a9b230fa4154515298bddf11614da70
Michel Pelletier <michelp@users.noreply.github.com>
2022-11-22 11:37:02 -0800

    add GA test runner. (#6)

--- test.sql
+++ test.sql
@@ -2,10 +2,8 @@
 BEGIN;
 
 CREATE EXTENSION pgtap;
-CREATE SCHEMA pgsodium;
-CREATE EXTENSION pgsodium WITH SCHEMA pgsodium CASCADE;
-
-CREATE EXTENSION supabase_vault CASCADE;
+CREATE EXTENSION pgsodium;
+CREATE EXTENSION supabase_vault;
 
 select plan(3);
 
@@ -14,22 +12,22 @@ SET ROLE postgres;
 SELECT lives_ok(
   format(
     $test$
-    INSERT INTO vault.secrets (secret, associated) VALUES ('s3kr3t_k3y', 'this is the foo api key');
+    INSERT INTO vault.secrets (secret, description) VALUES ('s3kr3t_k3y', 'this is the foo api key');
     $test$),
     'can insert into foo table');
 
 SELECT results_eq(
-    $$SELECT decrypted_secret = 's3kr3t_k3y', associated = 'this is the foo api key' FROM pgsodium_masks.secrets$$,
+    $$SELECT decrypted_secret = 's3kr3t_k3y', description = 'this is the foo api key' FROM vault.decrypted_secrets$$,
     $$VALUES (true, true)$$,
     'can select from masking view');
 
-UPDATE vault.secrets SET associated = 'bad';
+UPDATE vault.secrets SET description = 'bad';
 
 SELECT throws_ok(
-  $$SELECT decrypted_secret = 's3kr3t_k3y' FROM pgsodium_masks.secrets$$,
+  $$SELECT decrypted_secret = 's3kr3t_k3y' FROM vault.decrypted_secrets$$,
   '22000',
   'pgsodium_crypto_aead_det_decrypt_by_id: invalid ciphertext',
-  'mutated associated data fails decryption');
+  'mutated description data fails decryption');
 
 TRUNCATE vault.secrets;
 
